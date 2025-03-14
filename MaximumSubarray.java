3c06b1bc75191a6454d9c4d0e34dc39af590e81d
victor <gfeng005@uottawa.ca>
2025-03-14 13:13:33 -0400

    added dynamic programming solution for the maximum subarray.

--- MaximumSubarray.java
+++ MaximumSubarray.java
@@ -10,8 +10,39 @@ public class MaximumSubarray {
         }
         return max;
     }
+
+    public static int solution2(int[] nums) {
+        int[] dp = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            dp[i] = Integer.MIN_VALUE;
+        }
+        dpTopDown(nums, nums.length -1, dp);
+
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < dp.length; i++) {
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+
+    public static int dpTopDown(int[] nums, int index, int[] dp) {
+        if (index == 0) {
+            dp[0] = nums[0];
+            return nums[0];
+        }
+
+        int current;
+        if (dp[index -1] != Integer.MIN_VALUE) {
+            current =dp[index -1];
+        }
+
+        current = dpTopDown(nums, index -1, dp);
+        dp[index] = current < 0 ? nums[index] : current + nums[index];
+        return dp[index];
+    }
+
     public static void main(String[] args) {
         int[] nums = new int[] {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-        System.out.println(solution(nums));
+        System.out.println(solution2(nums));
     } 
 }
